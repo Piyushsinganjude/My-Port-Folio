2e4b4f1f64868f63d3593db260fbb55093a45b7d
piyushsinganjude <piyushsinganjude007@gmail.com>
2023-09-15 17:20:53 +0530

    initials

---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Piyush",
+		name: "Piyush.S",
+		email: "piyushsinganjude007@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/Piyushsingnjud7",
+		github: "https://github.com/Piyushsinganjude",
+		linkedin: "https://www.linkedin.com/in/piyush-singanjude-532797209/",
+		instagram: "https://www.instagram.com/piyush._.singanjude/",
+		stackoverflow: "https://stackoverflow.com/users/22018463/piyush-singanjude",
+		facebook: "https://www.facebook.com/piyush.singanjude/",
+	},
+
+	homepage: {
+		title: "Web Developer.",
+		description:
+			"I am a frontend developer with expertise in Node.js. I have experience in building fast, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Piyush Singanjude I live in Pune, where I design the Projects.",
+		description:
+			"I've worked on a variety of projects and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "AI-Image Generation",
+			description:
+				"It will use AI to generate images based on the details give about the image",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Piyushsinganjude/Dall-E_AI_IMAGE_GENERATOR",
+		},
+
+		{
+			title: "PDF to Audio with language Translation",
+			description:
+				"It will take PDF path file as an input and read the text in PDF to the User via Audio and also convert theselected text in desired language.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/Piyushsinganjude/PDF-to-Audio-convertor",
+		},
+
+		{
+			title: "AI- Summarizer",
+			description:
+				"It will give the summary of the complex news information into shorter and simple news information",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/Piyushsinganjude/AI_Summarizer",
+		},
+
+		{
+			title: "Chat-App",
+			description:
+				"It will enable the users to chat in private room. Firebase is used for the backend.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Piyushsinganjude/chat-app",
+		},
+
+		{
+			title: "CryptoBase Information",
+			description:
+				"It will give us all the current Details,current Price rate and current News about all the top cryptos in the world.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Piyushsinganjude/Cryptoinfo_App",
+		},
+	],
+};
+
+export default INFO;
